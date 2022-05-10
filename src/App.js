da06ec56fff6cd62f77cb1183aabb5aad201d1be
unknown <leonardoxavier092@gmail.com>
2022-05-09 22:53:01 -0300

    fix:bug value input

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const App = () => {
     <>
       <div className="container">
         <form>
-          <input type="text" value={result} />
+          <input type="text" onChange={(e) => e.target.value} value={result} />
         </form>
         <div className="keypad">
           <button className="highligth" onClick={clear} id="clear">
