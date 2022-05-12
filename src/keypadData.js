7763fa9715c2621c2afe13147c29c48dca7efef9
unknown <leonardoxavier092@gmail.com>
2022-05-12 00:47:54 -0300

    perf: create Keypad Data

---
+++ src/keypadData.js
@@ -0,0 +1,54 @@
+const keypad = [
+  {
+    value: '/',
+    class: 'highligth',
+  },
+  {
+    value: '7',
+  },
+  {
+    value: '8',
+  },
+  {
+    value: '9',
+  },
+  {
+    value: '*',
+    class: 'highligth',
+  },
+  {
+    value: '4',
+  },
+  {
+    value: '5',
+  },
+  {
+    value: '6',
+  },
+  {
+    value: '-',
+    class: 'highligth',
+  },
+  {
+    value: '1',
+  },
+  {
+    value: '2',
+  },
+  {
+    value: '3',
+  },
+  {
+    value: '+',
+    class: 'highligth',
+  },
+  {
+    value: '0',
+  },
+  {
+    value: '.',
+    class: 'highligth',
+  },
+];
+
+export default keypad;
