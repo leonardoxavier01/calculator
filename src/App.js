e6f4d2bc12bdf425b7761eba78d125f737b9e5d8
unknown <leonardoxavier092@gmail.com>
2022-05-12 00:49:14 -0300

    refactor: button key data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { clear } from '@testing-library/user-event/dist/clear';
 import React, { useState } from 'react';
 import './App.css';
+import keypad from './keypadData.js';
 
 const App = () => {
   const [result, setResult] = useState('');
@@ -32,57 +32,17 @@ const App = () => {
           <input type="text" onChange={(e) => e.target.value} value={result} />
         </form>
         <div className="keypad">
-          <button className="highligth" onClick={clear} id="clear">
+          <button className="highligth" id="clear" onClick={clear}>
             Clear
           </button>
-          <button className="highligth" onClick={backspace} id="backspace">
+          <button className="highligth" onClick={backspace}>
             C
           </button>
-          <button className="highligth" name="/" onClick={handleClick}>
-            &divide;
-          </button>
-          <button name="7" onClick={handleClick}>
-            7
-          </button>
-          <button name="8" onClick={handleClick}>
-            8
-          </button>
-          <button name="9" onClick={handleClick}>
-            9
-          </button>
-          <button className="highligth" name="*" onClick={handleClick}>
-            &times;
-          </button>
-          <button name="4" onClick={handleClick}>
-            4
-          </button>
-          <button name="5" onClick={handleClick}>
-            5
-          </button>
-          <button name="6" onClick={handleClick}>
-            6
-          </button>
-          <button className="highligth" name="-" onClick={handleClick}>
-            &ndash;
-          </button>
-          <button name="1" onClick={handleClick}>
-            1
-          </button>
-          <button name="2" onClick={handleClick}>
-            2
-          </button>
-          <button name="3" onClick={handleClick}>
-            3
-          </button>
-          <button className="highligth" name="+" onClick={handleClick}>
-            +
-          </button>
-          <button name="0" onClick={handleClick}>
-            0
-          </button>
-          <button className="highligth" name="." onClick={handleClick}>
-            .
-          </button>
+          {keypad.map((key, index) => (
+            <button className={key.class} name={key.value} onClick={handleClick} key={index}>
+              {key.value}
+            </button>
+          ))}
           <button className="highligth" onClick={calculate} id="result">
             =
           </button>
